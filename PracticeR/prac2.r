6db3a4d1eed988c4975f13fad605ae759b017d06
Cristian Paul <cristianabulencia13@gmail.com>
2025-12-12 23:43:17 +0800

    Deleted specific file

--- PracticeR/prac2.r
+++
@@ -1,9 +0,0 @@
-nums <- c(2,4,8)
-
-if (all(nums %% 2 == 0)) {
-    print("All numbers are even")
-} else if (any(nums %% 2 == 0)) {
-    print("Atleast one number is even")
-} else {
-    print("Ivalid")
-}
--- PracticeR/prac3.r
+++
@@ -1,9 +0,0 @@
-scores <- as.numeric(readline("Enter Grade: "))
-
-if (all(scores > 60)) {
-    print("All Students passed!")
-} else if (any(scores < 60)) {
-    print("At least one student failed!")
-} else {
-    print("Invalid!")
-}
